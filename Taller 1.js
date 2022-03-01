497598374cad1d80d3aacef51fc4e1b50b0328be
Francisco <franciscodavidtorres0109@gmail.com>
2022-03-01 10:52:09 -0500

    taller 1

---
+++ Taller 1.js
@@ -0,0 +1,17 @@
+/*Taller 1
+Fecha 24/02/2022
+Realizado por: Francisco David Torres
+*/
+var nombre; 
+console.log(nombre);
+//es de un tipo String debido a que guarda las cadenas de texto
+var gradoEstudiante;
+console.log("el grado es" + gradoEstudiante)
+//es de un tipo Double debido a que almacena numeros reales
+var edad = 16
+var edadEstudiante = edad
+//esta tambien es de un tipo Double debido a que almacena numeros reales
+nombre=
+
+
+
---
+++ index.js
@@ -0,0 +1,58 @@
+console.log("hola mundo");//imprimir por consola
+// comentario en linea
+// comentario
+// en
+// varias
+/*lineas
+o
+multilineas
+*/
+
+//variables
+/*son espacios de memoria que almacenan datos o informacion
+existen varios tipos de variables en js, pero generalmentese utilizan los siguientes:
+String: almacena cadenas de texto. Se debe declarar o asignar el valor entre comillas
+Int: almacena numeros enteros
+Double: almacena numeros reales
+Otro tipo de datos
+Boolean: almacena valores de verdad (true o false)
+*/
+/*declarar variables
+declarar una variable es crearla, decirle al lenguaje como se llama la variable e indicarle que ya existe
+Para declarar una variable se puede utilizar ya sea la palabra reservada Var o Let
+*/
+var nombre;
+console.log(nombre);
+/*inicializar variables
+para inicializar variables es necesario asignar un valor cuando se crea esta variable
+*/
+var apellido=" Torres";
+console.log(apellido);
+/*asignar un valor:
+asignar un valor se hace mediante el signo igual, se debe hacer sobre una variable que ya fue declarada
+*/
+nombre="Francisco David"
+console.log(nombre + "" + apellido)
+//Desde otra variable
+var edad = 15;
+var edadEstudiante = edad;
+//Desde el teclado
+var gradoEstudiante= prompt("ingrese el grado: ");
+console.log("el grado es: "+ gradoEstudiante);
+/*constantes:
+son valores que una vez inicializados no pueden cambiar lo que almacenan. Se crean con la palabra reservada const, el nombre de las constantes se escribe todo en mayuscula para diferenciarlo de las variables 
+*/
+const PI=3.1416
+//PI=5;
+//imprimir por consola
+//simple 
+console.log("mensaje") 
+console.log(nombre)
+//concatenado
+console.log("la edad es "+edad+" años");
+
+
+
+
+
+
